980aff05eb7eac497878a07b877a64fcfc4d2692
rosa lee <rosalee0116@gmail.com>
2020-04-08 19:29:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,9 @@ class Anagram
   end
 
   def match(array_of_words)
-    array_of_words.select do|element|(@word.split(" ").sort)==
-      (elements.split(" ").sort)
+    array_of_words.select do|element|
+      (@word.split("").sort) ==
+      (element.split("").sort)
     end
   end
 end
